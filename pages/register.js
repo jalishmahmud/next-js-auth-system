28c3976d4b8bd2021b7a1750768598b15cfd18cf
jalishmahmud <jalish93@gmail.com>
2023-04-06 14:11:16 +0600

    project setup with tailwind css,login and register route done

---
+++ pages/register.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import React from 'react'
+
+export default function Register() {
+    return (
+      
+        <div>
+            <Head>
+                <title>Register</title>
+            </Head>
+          <h2>Register</h2>
+    </div>
+  )
+}
